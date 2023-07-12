b0eaf549c60df36daca6201e7bef648a4d62c3d8
Roberto Salgado <roberto.salgado@unosquare.com>
2023-07-12 13:17:10 -0500

    Setup Cypress config to make use of cypress-cucumber-preprocessor plugin, according to the docs.

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+
+import preprocessor from "@badeball/cypress-cucumber-preprocessor";
+const { addCucumberPreprocessorPlugin } = preprocessor;
+
+import createESBuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    specPattern: "**/*.feature",
+    async setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on("file:preprocessor",
+        createBundler({
+          plugins: [createESBuildPlugin.default(config)],
+        })
+      );
+      
+      // Return the config object as it might have been modified by the plugin.
+      return config;
     },
   },
 });
